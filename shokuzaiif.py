49298a0dced59eaa5413d5100b05a3f715b1aac6
UEKI Miyu <21716017@jwu.ac.jp>
2020-01-16 16:01:00 +0900

    shokuzai2

--- shokuzaiif.py
+++ shokuzaiif.py
@@ -1,7 +1,19 @@
 
+#while True:
+ #   s = input("冷蔵庫の中に何がありますか？")
+  #  if(s):
+   #     print(s, "ですね。")
+    #else:
+     #   break
+
+
+#食べ物と個数を入力する
 while True:
-    s = input("冷蔵庫の中に何がありますか？")
-    if(s):
-        print(s, "ですね。")
+
+    s = input("食材と個数を入力してください(yes or no)")
+    if(s == 'yes'):
+        a,b=input().split()
+        print(a)
+        print(b)
     else:
         break
